e022fb6a849822f914b78a1ebe703383ddfb6d7e
ALIYU ABUBAKAR GARBA <sadiqful@gmail.com>
2019-03-11 16:03:32 +0100

    nuhu

--- y/js/grid.js
+++ y/js/grid.js
@@ -1,12 +1,3 @@
-/*
-* debouncedresize: special jQuery event that happens once after a window resize
-*
-* latest version and complete README available on Github:
-* https://github.com/louisremi/jquery-smartresize/blob/master/jquery.debouncedresize.js
-*
-* Copyright 2011 @louis_remi
-* Licensed under the MIT license.
-*/
 var $event = $.event,
 $special,
 resizeTimeout;
@@ -39,20 +30,7 @@ $special = $event.special.debouncedresize = {
 	threshold: 250
 };
 
-// ======================= imagesLoaded Plugin ===============================
-// https://github.com/desandro/imagesloaded
 
-// $('#my-container').imagesLoaded(myFunction)
-// execute a callback when all images have loaded.
-// needed because .load() doesn't work on cached images
-
-// callback function gets image collection as argument
-//  this is the container
-
-// original: MIT license. Paul Irish. 2010.
-// contributors: Oren Solomianik, David DeSandro, Yiannis Chatzikonstantinou
-
-// blank image data-uri bypasses webkit log warning (thx doug jones)
 var BLANK = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
 
 $.fn.imagesLoaded = function( callback ) {
